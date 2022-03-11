78288d5d5e3f8ee2b822b4333d0d9332070a1d9c
devendra <devendrachitanna@gmail.com>
2022-03-10 20:15:31 -0500

    useState example

---
+++ javascriptmastery/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+
+
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
+const App = () => {
+const dev = "devendra";
+const isnameShowing = true;
+const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+
+      <Person name={"johnny"}/>
+      <Person name={"devendra"} lastName={"chitanna"} age={12+13}/>
+      <h1>Hello, World!, {isnameShowing ? dev : "someone"}</h1>
+      {dev ? (
+        <>
+          test
+        </>
+      ) : (
+        <>
+        <h1>test</h1>
+        <h2>There is no name</h2>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
